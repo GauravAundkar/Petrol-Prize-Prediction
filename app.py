71bb4311f54c7a2322a36213f200056130e3ef1c
GauravAundkar <99090647+GauravAundkar@users.noreply.github.com>
2022-08-03 00:50:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+
+from flask import Flask, render_template, request
+
+import pandas as pd
+import petrol_price_prediction
+app = Flask(__name__,template_folder="templates",static_folder="static")
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.route("/result", methods = ["POST"])
+def submit():
+    if request.method =="POST":
+        dollar = request.form["dollar"]
+        barrel = request.form["barrel"]
+
+        # price= petrol_price_prediction.model.predict([[dollar, barrel]])
+        # arr = [price]
+        # res = pd.to_numeric(arr)
+        # print(price)
+
+    return render_template("result.html",a = dollar, b=barrel, c = 80.39)
+
+if __name__=="__main__":
+    app.run(debug=True)
